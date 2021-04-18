8fc3ce993010fa22089b0d1fc5a3bcd1cfd9ae03
Colin Varney <cvcolinv@gmail.com>
2021-04-18 01:17:36 -0400

    Added useEffect hook for reactive rendering on state changes.

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+  const [times, setTimes] = useState(0)
+ 
     
     function increment() {
         setCount(count +1)
@@ -15,8 +17,14 @@ function Counter() {
     }
     function decrementTwo() {
       setCount(count - 2);
-    }
+  }
+  function addToTimes() {
+    setTimes(times + 1);
+  }
     
+  
+  
+  useEffect(addToTimes, [count])
 
 
     return (
@@ -24,9 +32,11 @@ function Counter() {
         <h1>The Current Number is: {count}</h1>
         <br />
         <button onClick={incrementTwo}> +2</button>
-            <button onClick={increment}> +1</button>
+        <button onClick={increment}> +1</button>
         <button onClick={decrement}> -1</button>
         <button onClick={decrementTwo}> -2</button>
+
+        <h3> You've changed the count: {times -1} times! </h3>
       </div>
     );
 }
